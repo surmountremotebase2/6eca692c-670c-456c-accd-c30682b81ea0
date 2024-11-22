1d8d1d689bd99bbc05f131ee2313309b16008343
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-22 08:00:29 +0500

    User Rutvik Patel saved changes to multiple files files at 11/22/2024, 03:00:28 UTC time.

--- c1ba0b8d-9e5e-4fc0-b9f1-16a63f072718/main.py
+++ c1ba0b8d-9e5e-4fc0-b9f1-16a63f072718/main.py
@@ -25,7 +25,7 @@ class TradingStrategy(Strategy):
 
         # Compute the MACD for SPY. Here we're using a standard fast=12, slow=26 period configuration.
         macd_result = MACD("SPY", data["ohlcv"], 12, 26)
-        log("Hello world")
+        log("Hello world", macd_result)
 
         if macd_result is not None:
             # Extract the MACD line, Signal line, and Histogram
