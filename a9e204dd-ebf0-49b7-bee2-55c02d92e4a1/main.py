37a4124561e22a48305261a068bef471738c6e1e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-23 07:29:25 +0500

    User Rutvik Patel saved changes to multiple files files at 11/23/2024, 02:29:24 UTC time.

--- a9e204dd-ebf0-49b7-bee2-55c02d92e4a1/main.py
+++ a9e204dd-ebf0-49b7-bee2-55c02d92e4a1/main.py
@@ -1,1 +1,62 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import MACD, RSI
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    # Define the assets this strategy will trade
+    @property
+    def assets(self):
+        return ["SPY", "SQQQ"]
+
+    # Set the interval for the data. This strategy uses 5-minute intervals.
+    @property
+    def interval(self):
+        return "5min"
+
+    def run(self, data):
+        """
+        Execute the trading strategy for SPY based on MACDs value.
+
+        :param data: The market data provided by the Surmount trading environment.
+        :return: A TargetAllocation object defining the target allocations for the assets.
+        """
+        # Initialize allocation to the current holdings or default to 0
+        holdings = data["holdings"]
+        allocation = {"SPY": holdings.get("SPY", 0), "SQQQ": holdings.get("SQQQ", 0)}
+        
+
+        # Compute the MACD for SPY. Here we're using a standard fast=12, slow=26 period configuration.
+        macd_result = MACD("SPY", data["ohlcv"], 12, 26)
+        rsi_result = RSI("SPY", data, 14)
+
+        if macd_result:
+            # Extract the Signal line (MACDs) from the returned dictionary
+            signal_line = macd_result.get("MACDs_12_26_9", [])
+            macd_line = macd_result.get("MACD_12_26_9", [])
+
+            rsi_data = rsi_result[-3:]
+        
+    
+            slope_1 = (rsi_data[1] - rsi_data[0]) / 1
+            slope_2 = (rsi_data[2] - rsi_data[1]) / 1
+            
+            rsi_slope =  (slope_1 + slope_2) / 2
+            
+            current_diff = macd_line[-1] - signal_line[-1]
+            previous_diff = macd_line[-2] - signal_line[-2]
+            
+            if rsi_slope < -3:
+                allocation = 1.0
+            elif macd_line[-2] < signal_line[-2] and macd_line[-1] > signal_line[-1]:
+                log("Bullish crossover detected: MACD Line has crossed above Signal Line. rut")
+                allocation["SPY"] = 1.0  # Allocate 100% to SPY
+                
+            # Check for bearish convergence
+            if current_diff > 0 and previous_diff > current_diff:
+                log("Bearish convergence detected: MACD is moving closer to Signal Line. rut SQQQ")
+                allocation["SQQQ"] = 0.5  # Set SQQQ allocation to 50%
+                allocation["SPY"] = 0.0  # Exit SPY
+           
+
+        # Return the allocation advisory for SPY
+        return TargetAllocation(allocation)
