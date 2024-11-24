286312e9ff18fd739a35b4751921e4c7d34ee980
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-24 05:12:29 +0500

    User Rutvik Patel saved changes to multiple files files at 11/24/2024, 00:12:29 UTC time.

--- a9e204dd-ebf0-49b7-bee2-55c02d92e4a1/main.py
+++ a9e204dd-ebf0-49b7-bee2-55c02d92e4a1/main.py
@@ -45,10 +45,7 @@ class TradingStrategy(Strategy):
             current_diff = macd_line[-1] - signal_line[-1]
             previous_diff = macd_line[-2] - signal_line[-2]
             
-            if rsi_slope < -3:
-                allocation = 1.0
-                sq_allocation = 0.0
-            elif macd_line[-2] < signal_line[-2] and macd_line[-1] > signal_line[-1]:
+            if macd_line[-2] < signal_line[-2] and macd_line[-1] > signal_line[-1]:
                 log("Bullish crossover detected: MACD Line has crossed above Signal Line.")
                 allocation = 1.0  # Allocate 100% to SPY
                 sq_allocation = 0.0
