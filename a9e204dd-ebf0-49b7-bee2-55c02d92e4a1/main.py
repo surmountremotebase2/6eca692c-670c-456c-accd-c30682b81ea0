e22c92372836356ff909847afce799a01cd355d5
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-24 06:20:00 +0500

    User Rutvik Patel saved changes to multiple files files at 11/24/2024, 01:19:59 UTC time.

--- a9e204dd-ebf0-49b7-bee2-55c02d92e4a1/main.py
+++ a9e204dd-ebf0-49b7-bee2-55c02d92e4a1/main.py
@@ -30,7 +30,7 @@ class LongHoldingTradingStrategy(Strategy):
         macd_result = MACD("SPY", data["ohlcv"], 12, 26)
         rsi_result = RSI("SPY", data, 14)
         ema_50 = EMA("SPY", data["ohlcv"], 50)[-1] if EMA("SPY", data["ohlcv"], 50) else None
-        current_price = data["ohlcv"][-1]["SPY"]["close"]
+        current_price = data["ohlcv"]["SPY"]["close"]
 
         if macd_result and rsi_result and ema_50:
             # Extract MACD and Signal Line
